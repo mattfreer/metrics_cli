a390906e2d4b55ceaaab2b768b8feab1eb4da442
Matt Freer <matt.freer@gmail.com>
2013-04-16 10:51:43 +0100

    Removed unused step_definitions

--- features/step_definitions/metrics_steps.rb
+++
@@ -1,6 +0,0 @@
-#Then /^it shall print metrics to the standard out$/ do
-  #assert_partial_output('Foobar', "sas")
-#end
-#Then /^the stdout should contain "([^"]*)"$/ do |string|
-  #@output.should include string
-#end
